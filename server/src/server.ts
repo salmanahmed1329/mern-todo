edd373acf58734803147ea5a0db424fcad45eaf3
salmanahmed1329 <salmanahmed1329@gmail.com>
2023-05-11 15:59:51 +0500

    created Todo controller endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,6 +18,94 @@ dotenv.config();
 
 await DB_Connect();
 
+//------------------------------------------------ Todo Controller... ------------------------------------------------//
+
+server.get('/todos', async (_, response) => 
+{
+  const todosList = await Todo.find().lean().exec();
+
+  response.json(todosList?.length ? todosList : []);
+});
+
+server.post('/todo', async (request, response) => 
+{
+  const todo = await (new Todo(request?.body)).save();
+
+  response.json(todo?._id ? todo : { error: `todo item not created...` });
+});
+
+server.get('/todo/:id', async (request, response) => 
+{
+  const todo = await Todo.findById(request.params?.id);
+
+  if (todo?._id)
+  {
+    response.json(todo);
+  }
+
+  else 
+  {
+    response.status(404).json({ error: `todo item not found...` });
+  }
+});
+
+server.put('/todo/:id', async (request, response) => 
+{
+  const todo = await Todo.findById(request.params?.id);
+
+  if (todo?._id)
+  {
+    todo.task = request.body?.task;
+
+    await todo?.save();
+
+    response.json(todo);
+  }
+
+  else 
+  {
+    response.status(404).json({ error: `todo item not found...` });
+  }
+});
+
+server.delete('/todo/:id', async (request, response) => 
+{
+  const todo = await Todo.findById(request.params?.id);
+
+  if (todo?._id)
+  {
+    todo.task = request.body?.task;
+
+    await todo?.deleteOne();
+
+    response.json(todo);
+  }
+
+  else 
+  {
+    response.status(404).json({ error: `todo item not found...` });
+  }
+});
+
+server.get('/todo/toggle/:id', async (request, response) => 
+{
+  const todo = await Todo.findById(request.params?.id);
+
+  if (todo?._id)
+  {
+    todo.isFinished = !todo.isFinished;
+
+    await todo?.save();
+
+    response.json(todo);
+  }
+
+  else 
+  {
+    response.status(404).json({ error: `todo item not found...` });
+  }
+});
+
 //------------------------------------------------ Server Bootup... ------------------------------------------------//
 
 server.listen
