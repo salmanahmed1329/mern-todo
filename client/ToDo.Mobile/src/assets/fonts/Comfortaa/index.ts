d1a0788fcf5d22996b097e37eb92ec9d761f0034
salmanahmed1329 <salmanahmed1329@gmail.com>
2023-05-18 09:06:35 +0500

    added 'Comfortaa' font assets

---
+++ client/ToDo.Mobile/src/assets/fonts/Comfortaa/index.ts
@@ -0,0 +1,8 @@
+export default 
+{
+  Font_Bold: "Comfortaa-Bold",
+  Font_Light: "Comfortaa-Light",
+  Font_Medium: "Comfortaa-Medium",
+  Font_Regular: "Comfortaa-Regular",
+  Font_SemiBold: "Comfortaa-SemiBold"
+}
